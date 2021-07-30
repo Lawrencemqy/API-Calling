5bf1b891556fc01850ec77da6134354138f0e74c
Lawrencemqy <1346847095@qq.com>
2021-07-30 13:34:52 +0800

    Initial Commit

---
+++ API Calling/API_CallingApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_CallingApp.swift
+//  API Calling
+//
+//  Created by 梅清阳 on 2021/7/30.
+//
+
+import SwiftUI
+
+@main
+struct API_CallingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ API Calling/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  API Calling
+//
+//  Created by 梅清阳 on 2021/7/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
